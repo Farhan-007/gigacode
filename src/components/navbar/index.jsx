35cd3405cbf8adc45d450a8b1d3ec0ac5ef4b225
Farhan akhtar <sheikhfarhan722@gmail.com>
2022-10-14 21:31:00 +0530

    landing page and navbar added

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,20 @@
+import "./style.css"
+
+export default function Header() {
+   
+    return (
+        <div className="headWrapper fixed box-border flex items-center bg-white h-[10vh] w-[100vw]">
+            <div className="header flex mx-[16vh] w-[100vw] justify-between ">
+                <div className="logo flex items-baseline text-2xl">GigaCode <span className=" block mx-1 bg-green-500 w-[10px] h-[15px]"></span></div>
+                <div className="nav">
+                    <ul className="flex py-1">
+                        <li className=" px-10 py-1">Login</li>
+                        <li className=" px-10  py-1 border-solid border-gray-500 border-[1px]">Sign Up</li>
+                    </ul>
+                </div>
+
+            </div>
+            
+        </div>
+    )
+}
