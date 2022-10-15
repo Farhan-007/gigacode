25740ca5b88c264c8dc9159c3131c160230a1e9b
Farhan akhtar <sheikhfarhan722@gmail.com>
2022-10-15 14:14:40 +0530

    first push

--- src/components/landing/index.jsx
+++ src/components/landing/index.jsx
@@ -3,7 +3,7 @@ import "./style.css"
 export default function Landing() {
 
     return (
-        <div className="flex box-border w-[100vw] h-[100vh]">
+        <div className="flex box-border w-[100vw] h-[105vh]">
             <div className="w-1/2 flex flex-col mt-[10vh] items-center justify-between py-28">
                 <div className=" text-3xl flex flex-col w-3/4 h-1/4 text-slate-500 text-center justify-end">
                     <h1>Code, Compete</h1>
@@ -16,12 +16,12 @@ export default function Landing() {
                     </p>
                 </div>
                 <div className="text-center text-2xl w-3/4 h-1/4 ">
-                    <button className=" border-[1px] px-20 py-5 rounded-[20px] bg-red-600 text-white">
+                    <button className=" border-none px-6 py-3  bg-green-500 text-white">
                         Register Now
                     </button>
                 </div>
             </div>
-            <div className="w-1/2 flex items-center justify-center bg-orange-500 ">
+            <div className="w-1/2 flex items-center justify-center  ">
                 <h1 className=" text-4xl">idhar mst photu daalne ka re baba</h1>
             </div>
         </div>
--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -3,8 +3,8 @@ import "./style.css"
 export default function Header() {
    
     return (
-        <div className="headWrapper fixed box-border flex items-center bg-white h-[10vh] w-[100vw]">
-            <div className="header flex mx-[16vh] w-[100vw] justify-between ">
+        <div className="headWrapper fixed box-border flex items-center bg-white h-[10vh] w-[100vw] z-10">
+            <div className="header flex mx-[8vw] w-[100vw] justify-between ">
                 <div className="logo flex items-baseline text-2xl">GigaCode <span className=" block mx-1 bg-green-500 w-[10px] h-[15px]"></span></div>
                 <div className="nav">
                     <ul className="flex py-1">
